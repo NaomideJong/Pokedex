d8260e5d1aff902743dbb77990aaab6ab3088510
Jonah Juliao Toral <j.toral@johnstonsofelgin.com>
2024-10-03 08:14:03 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jonah Juliao Toral on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Jonah Juliao Toral on 03/10/2024.
+//
+
+import Testing
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
